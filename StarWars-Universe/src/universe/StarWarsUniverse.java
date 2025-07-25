8e431ef74f0ebd6583c7f147b8b9cce334fe3d4e
izxga <mantarov.andy@gmail.com>
2025-07-25 17:16:10 +0300

    Added specifics to class StarWarsUniverse

--- StarWars-Universe/src/universe/StarWarsUniverse.java
+++ StarWars-Universe/src/universe/StarWarsUniverse.java
@@ -1,4 +1,27 @@
 package universe;
 
+import model.Planet;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class StarWarsUniverse {
+    private Map<String, Planet> planets = new HashMap<>();
+
+    public void addPlanet(String name) {
+        if (!planets.containsKey(name)) {
+            planets.put(name, new Planet(name));
+            System.out.println("Planet " + name + " added.");
+        } else {
+            System.out.println("Planet already exists.");
+        }
+    }
+
+    public Planet getPlanet(String name) {
+        return planets.get(name);
+    }
+
+    public void printUniverse() {
+        planets.values().forEach(System.out::println);
+    }
 }
