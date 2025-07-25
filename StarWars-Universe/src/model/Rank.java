b051bb58689b8bdaa11f0fc70c134477916d72be
izxga <mantarov.andy@gmail.com>
2025-07-25 17:27:33 +0300

    Revert "Added specifics to class Rank."
    This reverts commit 134f27727ddc876bdbb36f406d2356c328cbd8cc.

--- StarWars-Universe/src/model/Rank.java
+++ StarWars-Universe/src/model/Rank.java
@@ -1,12 +1,4 @@
 package model;
 
-public enum Rank {
-    YOUNGLING,
-    INITIATE,
-    PADAWAN,
-    KNIGHT_ASPIRANT,
-    KNIGHT,
-    MASTER,
-    BATTLE_MASTER,
-    GRAND_MASTER;
+public class Rank {
 }
