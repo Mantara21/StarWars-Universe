23035b5d3f8f0d91048054542d5db1143e743c4b
izxga <mantarov.andy@gmail.com>
2025-07-25 17:27:23 +0300

    Revert "Added specifics to class Planet."
    This reverts commit 13e3e41faac6d81d9804ba909a5c88a6fd58a376.

--- StarWars-Universe/src/model/Planet.java
+++ StarWars-Universe/src/model/Planet.java
@@ -1,30 +1,4 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Planet {
-    private String name;
-    private List<Jedi> jedis;
-
-    public Planet(String name) {
-        this.name = name;
-        this.jedis = new ArrayList<>();
-    }
-
-    public String getName() { return name; }
-    public List<Jedi> getJedis() { return jedis; }
-
-    public void addJedi(Jedi jedi) {
-        jedis.add(jedi);
-    }
-
-    public void removeJedi(Jedi jedi) {
-        jedis.remove(jedi);
-    }
-
-    @Override
-    public String toString() {
-        return name + " with " + jedis.size() + " jedis.";
-    }
 }
