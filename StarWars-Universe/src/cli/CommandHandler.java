b2dad62827d31d9de5ebdb9727fb6a6a03d6d4a9
izxga <mantarov.andy@gmail.com>
2025-07-27 19:25:41 +0300

    Added specifics to classes CommandHandler and FileManager

--- StarWars-Universe/src/cli/CommandHandler.java
+++ StarWars-Universe/src/cli/CommandHandler.java
@@ -1,4 +1,71 @@
 package cli;
 
+import io.FileManager;
+import universe.StarWarsUniverse;
+
+import java.util.Scanner;
+
 public class CommandHandler {
+    private StarWarsUniverse universe = new StarWarsUniverse();
+    private FileManager fileManager = new FileManager();
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        String command;
+
+        System.out.println("Welcome to the Star Wars Universe CLI!");
+        while (true) {
+            System.out.print("> ");
+            command = scanner.nextLine().trim();
+
+            if (command.equalsIgnoreCase("exit")) {
+                System.out.println("Exiting the program...");
+                break;
+            }
+
+            handleCommand(command);
+        }
+    }
+
+    private void handleCommand(String command) {
+        String[] parts = command.split(" ");
+        switch (parts[0].toLowerCase()) {
+            case "open":
+                if (parts.length >= 2) {
+                    fileManager.open(parts[1], universe);
+                } else {
+                    System.out.println("Usage: open <file>");
+                }
+                break;
+            case "close":
+                fileManager.close();
+                break;
+            case "save":
+                fileManager.save();
+                break;
+            case "saveas":
+                if (parts.length >= 2) {
+                    fileManager.save(universe, parts[1]);
+                } else {
+                    System.out.println("Usage: saveas <file>");
+                }
+                break;
+            case "help":
+                printHelp();
+                break;
+            default:
+                System.out.println("Unknown command. Type 'help' for a list.");
+        }
+    }
+
+    private void printHelp() {
+        System.out.println("""
+                open <file> - opens file
+                close - closes current file
+                save - saves the current file
+                saveas <file> - saves current file as new file
+                help - shows this message
+                exit - exits the program
+                """);
+    }
 }
--- StarWars-Universe/src/io/FileManager.java
+++ StarWars-Universe/src/io/FileManager.java
@@ -1,4 +1,38 @@
 package io;
 
+import universe.StarWarsUniverse;
+
+import java.io.*;
+
 public class FileManager {
+    private File currentFile;
+
+    public boolean open(String filePath, StarWarsUniverse universe) {
+        currentFile = new File(filePath);
+        if (!currentFile.exists()) {
+            System.out.println("File not found. Creating new.");
+            return save(universe, filePath);
+        }
+        // TODO: Load data from file
+        System.out.println("Successfully opened " + filePath);
+        return true;
+    }
+
+    public boolean save(StarWarsUniverse universe, String filePath) {
+        // TODO: Save data to file
+        System.out.println("Successfully saved " + filePath);
+        return true;
+    }
+
+    public boolean save() {
+        if (currentFile != null) {
+            return save(null, currentFile.getPath());
+        }
+        return false;
+    }
+
+    public void close() {
+        currentFile = null;
+        System.out.println("Successfully closed file.");
+    }
 }
